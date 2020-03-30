ff94082ace0b01f98ed77a12ff7e51a3047c190a
mcobb12 <62885974+mcobb12@users.noreply.github.com>
2020-03-30 08:56:18 -0400

    owo

---
+++ hello_world/src/hello_world/main.java
@@ -0,0 +1,11 @@
+package hello_world;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message = "Hello World!";
+		System.out.println(message)
+	}
+
+}
